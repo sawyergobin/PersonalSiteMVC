bdafef8e23349911e3c3d7e2e3b90c82c05d838e
Sawyer Gobin <sawyergobin@outlook.com>
2021-03-07 13:52:39 -0600

    Added a functional contact form with Ajax

--- PersonalSiteMVC.UI.MVC/Controllers/HomeController.cs
+++ PersonalSiteMVC.UI.MVC/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using PersonalSiteMVC.UI.MVC.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 
@@ -13,6 +16,38 @@ namespace PersonalSiteMVC.UI.MVC.Controllers
             return View();
         }
 
-       
+        [ValidateAntiForgeryToken]
+        public JsonResult ContactAjax(ContactViewModel cvm)
+        {
+            //This is the body of the message sent
+            string body = $"{cvm.Name} has sent you the following message: <br />" +
+                $"{cvm.Message} <br /> " +
+                $"<strong>from the email address: {cvm.Email}</strong> ";
+
+            MailMessage m = new MailMessage("admin@sawyergobin.com", "sawyergobin@outlook.com", cvm.Subject, body);
+
+            m.IsBodyHtml = true;
+            m.Priority = MailPriority.High;
+            m.ReplyToList.Add(cvm.Email);
+
+            SmtpClient client = new SmtpClient("mail.sawyergobin.com");
+            //credentials
+            //++++++++++++++++++++++++++++++SENSITIVE CREDENTIALS: REMOVE BEFORE MAKING REPO PUBLIC+++++++++++++++++++++
+            client.Credentials = new NetworkCredential("admin@sawyergobin.com", "XWv8Mqe6Wt_");
+
+            try
+            {
+                //send mail
+                client.Send(m);
+            }
+            catch (Exception e)
+            {
+                ViewBag.Message = e.StackTrace;
+            }
+
+            return Json(cvm);
+
+        }
+        
     }
 }
---
+++ PersonalSiteMVC.UI.MVC/Models/ContactViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PersonalSiteMVC.UI.MVC.Models
+{
+    public class ContactViewModel
+    {
+
+
+        [Required(ErrorMessage = "* Name Is Required")]
+        [Display(Name = "Your Name")]
+        public string Name { get; set; }
+
+        [EmailAddress(ErrorMessage = "Please Enter A Valid Email")]
+        [Required(ErrorMessage = "* Email Is Required")]
+        [Display(Name = "Your Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage ="* Subject Is Required")]
+        public string Subject { get; set; }
+        [UIHint("MultilineText")]
+        [Required(ErrorMessage = "* A Message is required")]
+        public string Message { get; set; }
+    }
+}
